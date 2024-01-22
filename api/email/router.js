2a9a4fe37c4e58e125de0649b4159e719d6c305c
Langston Howley <ljhowley11@gmail.com>
2024-01-22 14:44:53 -0500

    Implement email handler

--- api/email/router.js
+++ api/email/router.js
@@ -1,19 +1,58 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const nodemailer = require("nodemailer");
+const router = express.Router();
 
+const AWS = require("aws-sdk");
+AWS.config.update({ region: "us-east-1" });
+const secretsManager = new AWS.SecretsManager();
 
-router.post('/', (req, res) => {
-    from = req.body.from || null;
-    message = req.body.message || null;
+var transporter = null;
+var myEmail = null;
 
-    if(!from || !message){
-        res.status(400)
-        res.json({ error: "from or message is null"})
-    }else{
-        console.log(from, message)
-        res.status(200)
-        res.json({status: 'you good'})
-    }
+secretsManager.getSecretValue({ SecretId: "portfolio" }, (err, data) => {
+  if (err) {
+    console.error("Email server couldn't get the portfolio secrets: ", err);
+  } else {
+    const secret = JSON.parse(data.SecretString);
+
+    myEmail = secret.email
+
+    transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: secret.email,
+        pass: secret.emailPass,
+      },
+    });
+  }
 });
 
-module.exports = router
+router.post("/", (req, res) => {
+  let from = req.body.from || null;
+  let message = req.body.message || null;
+
+  if (!from || !message) {
+    res.status(400).json({ error: "from or message is null" });
+  } else if (myEmail === null) {
+    console.log("Did you forget your email sir?");
+    res.status(500).json({ error: "Internal server Error" });
+  } else {
+    const mailOptions = {
+      from: from,
+      to: myEmail,
+      subject: "Message from langstonhowley.me",
+      text: message,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log("Email send error: ", error);
+        return res.status(500).send(`Internal Server Error`);
+      } else {
+        res.status(200).json({ status: "you good" });
+      }
+    });
+  }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,18 +1,12 @@
-// require("dotenv").config();
 const express = require(`express`);
 const app = express();
 const bodyParser = require("body-parser");
-const querystring = require("querystring");
 const cors = require("cors");
 const path = require("path");
-// const port = process.env.BACKEND_PORT || 6969;
 
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-1" });
 const secretsManager = new AWS.SecretsManager();
-
-const weather = require("./api/weather/router");
-const email = require("./api/email/router");
 
 app.use(cors());
 app.options("*", cors());
@@ -21,38 +15,36 @@ app.use(bodyParser.json());
 
 const parent_dir = path.resolve(__dirname, '..')
 const buildPath = path.join(parent_dir, "client/build");
-
 app.use(express.static(buildPath));
 
+var port = 6969; //Default port in case secrets don't work
 secretsManager.getSecretValue({ SecretId: "portfolio" }, (err, data) => {
   if (err) {
     console.error(err);
   } else {
-    // Parse the secret JSON data
     const secret = JSON.parse(data.SecretString);
-    const port = secret.port;
-
-    app.get("/*", function (req, res) {
-      console.log(buildPath)
-      res.sendFile(
-        path.join(buildPath, "index.html"),
-        function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-    });
-
-    app.use("/weather", weather);
-    app.use("/email", email);
-
-    app.get("/", (req, res) => {
-      res.send("Hello");
-    });
-
-    app.listen(port, () => {
-      console.log(`server is listening Port:`, port);
-    });
+    port = secret.port;
   }
+});
+
+const weather = require("./api/weather/router");
+const email = require("./api/email/router");
+
+app.get("/*", function (req, res) {
+  console.log(buildPath)
+  res.sendFile(
+    path.join(buildPath, "index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
+
+app.use("/weather", weather);
+app.use("/email", email);
+
+app.listen(port, () => {
+  console.log(`server is listening Port:`, port);
 });
