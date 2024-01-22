36c49bf2c266f62a5f32a213ac9b53f315a58685
Langston Howley <ljhowley11@gmail.com>
2024-01-22 12:13:03 -0500

    Cache weather values

--- api/weather/router.js
+++ api/weather/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const querystring = require("querystring");
+
+const NodeCache = require("node-cache");
+const weatherCache = new NodeCache({ stdTTL: 10000 });
+
 const AWS = require("aws-sdk");
 AWS.config.update({ region: "us-east-1" });
 const secretsManager = new AWS.SecretsManager();
@@ -9,19 +13,21 @@ router.post("/", (req, res) => {
   secretsManager.getSecretValue({ SecretId: "portfolio" }, (err, data) => {
     if (err) {
       res.status(500);
-      console.log(err);
+      console.log("Error in getting secret value: ", err);
       res.json({ error: "Server Error" });
     } else {
-      const secret = JSON.parse(data.SecretString);
-
       const city = req.body.city || null;
       const state = req.body.state || " ";
       const country = req.body.country || " ";
 
       if (city === null) {
-        res.status(400);
-        res.json({ error: "City cannot be empty" });
+        res.status(400).json({ error: "City cannot be empty" });
       } else {
+        if (weatherCache.has(city)) {
+          return res.status(200).json(weatherCache.get(city));
+        }
+
+        const secret = JSON.parse(data.SecretString);
         let url =
           "https://api.openweathermap.org/data/2.5/weather?" +
           querystring.stringify(
@@ -42,17 +48,15 @@ router.post("/", (req, res) => {
             errorMessage = responseJson.message || null;
 
             if (errorMessage === null) {
-              res.status(200);
-              res.json(responseJson);
+              weatherCache.set(city, responseJson);
+              res.status(200).json(responseJson);
             } else {
-              res.status(400);
-              res.json({ error: errorMessage });
+              res.status(400).json({ error: errorMessage });
             }
           })
           .catch((error) => {
-            res.status(500);
-            console.log(error);
-            res.json({ error: "Server Error" });
+            console.log("Error on weather image: ", error);
+            res.status(500).json({ error: "Server Error" });
           });
       }
     }
